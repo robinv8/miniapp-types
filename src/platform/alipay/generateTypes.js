530abd4197a899d9ec16d1ace1ebb687e298c794
Ren Yubin <robin@sifou.com>
2022-05-10 10:56:54 +0800

    refactor: 规则调整

--- src/platform/alipay/generateTypes.js
+++ src/platform/alipay/generateTypes.js
@@ -1,53 +1,130 @@
 const puppeteer = require('puppeteer');
-const fs = require('fs')
-const path = require('path')
-const humps = require('humps');
-const componentList = require('./componentList.json')
+const fs = require('fs');
+const path = require('path');
+const componentList = require('./componentList.json');
 
-
-const fn = async ()=>{
+const fn = async () => {
   const browser = await puppeteer.launch({
     headless: false,
     defaultViewport: null,
-  })
+  });
   const page = await browser.newPage();
 
-  let index=0
-  const getComponentProps = async (url, page)=>{
-    await page.goto(url,{ waitUntil: 'networkidle0' });
-    const data = await page.evaluate(()=>{
-      const tableList = document.querySelectorAll('table')
-      const attributes = []
-      console.log(tableList)
-      tableList.forEach(tableEle=>{
-        tableEle.querySelectorAll('tr')?.forEach(el=>{
-          if(el.childNodes.length===3 && el.childNodes[0].innerText!=='属性'){
+  let index = 0;
+  const getComponentProps = async ({ page, url, name }) => {
+    await page.goto(url, { waitUntil: 'networkidle0' });
 
-            attributes.push({
-              name: el.childNodes[0].innerText,
-              type: el.childNodes[1].innerText,
-              desc: el.childNodes[2].innerText
-            })
+    const data = await page.evaluate(({ name, tableIndex = 0, fields }) => {
+      function handleType(str, char) {
+        if (str.indexOf(char) === -1) {
+          return str;
+        }
+        return str.split(char).map((item) => item.trim());
+      }
+      const attributes = [];
+
+      const tablesEl = document.querySelectorAll('table')[tableIndex];
+      tablesEl?.querySelectorAll('tbody tr')?.forEach((el, index) => {
+        if (index === 0) {
+          return;
+        }
+        const len = el.children.length;
+        const options = [];
+
+        if (len > 0) {
+          const optEl = document.querySelector(
+            `.lake-engine-view>a[name='${el.children[0]?.innerText}-有效值']`,
+          );
+          if (optEl) {
+            optEl.nextElementSibling.nextElementSibling
+              .querySelectorAll('tbody tr')
+              .forEach((el, index) => {
+                if (index === 0) {
+                  return;
+                }
+                options.push(el.children[0].innerText);
+              });
           }
-        })
+          const description = el.children[fields.description]?.innerText;
 
-      })
-      return attributes
-    })
-    let tsCode = 'interface '+humps.pascalize(componentList[index].name)+'Props {\n'
-    data.forEach(el=>{
-      tsCode += ' '+humps.camelize(el.name)+': '+el.type+';\n'
-    })
-    tsCode += '}'
-    componentList[index].props=data
-    fs.writeFileSync(path.join(process.cwd(), `dist/alipay/${humps.camelize(componentList[index].name)}.d.ts`),tsCode)
-    index++
-    const nextUrl = componentList[index]?.url
-    if(nextUrl) {
-      getComponentProps(componentList[index].url,page)
+          const defaultValue = el.children[fields.defaultValue]?.innerText || description.match(/默认值： (\w+)/)?.[1];
+          const required = el.children[fields.required]?.innerText;
+
+          attributes.push({
+            name: el.children[fields.name]?.innerText,
+            type: handleType(el.children[fields.type]?.innerText, '/'),
+            ...(options.length > 0 ? { options } : {}),
+            ...(defaultValue ? { defaultValue } : {}),
+            ...(description ? { description } : {}),
+            ...(required !== undefined && required !== null
+              ? { required: required === '是' }
+              : {}),
+          });
+        }
+      });
+
+      return attributes;
+    }, componentList[index]);
+    if (data.length > 0) {
+      const folder = path.join(__dirname, 'jsonScheam');
+      if (!fs.existsSync(folder)) {
+        fs.mkdirSync(folder, { recursive: true });
+      }
+      fs.writeFileSync(
+        path.join(folder, `${name}.json`),
+        JSON.stringify(data, null, 2),
+      );
     }
-  }
-  getComponentProps(componentList[index].url,page)
-}
 
-fn()
+    // let tsCode =
+    //   'interface ' + humps.pascalize(componentList[index].name) + 'Props {\n';
+    // data.forEach((prop) => {
+    //   tsCode += `
+    //   /**
+    //    * @description ${prop.description}${
+    //     prop.defaultValue ? `\n* @default ${prop.defaultValue}` : ''
+    //   }
+    //    * @type ${humps.camelize(prop.type)}
+    //    */
+    //   ${humps.camelize(prop.name)}: ${
+    //     prop.options?.length > 0
+    //       ? prop.options.map((item) => `'${item}'`).join(' | ')
+    //       : humps.camelize(prop.type)
+    //   };
+    //   `;
+    // });
+    // tsCode += '}';
+    // if (!fs.existsSync('dist/qq')) {
+    //   fs.mkdirSync(path.join(process.cwd(), 'dist/qq'), { recursive: true });
+    // }
+
+    // fs.writeFileSync(
+    //   path.join(
+    //     process.cwd(),
+    //     `dist/qq/${humps.camelize(componentList[index].name)}.d.ts`,
+    //   ),
+    //   tsCode,
+    // );
+    index++;
+    const { url: nextUrl = '', name: nextName = '' } =
+      componentList[index] || {};
+    if (nextUrl) {
+      getComponentProps({
+        url: nextUrl,
+        name: nextName,
+        page,
+      });
+    } else {
+      await browser.close();
+    }
+  };
+  const { url = '', name } = componentList[index] || {};
+
+  getComponentProps({
+    url,
+    name,
+    page,
+  });
+};
+
+fn();
