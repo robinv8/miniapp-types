2a8ea17dc13e75451ed8d7f8175dc8ebcd005255
robin <robin@rnode.me>
2022-06-13 23:50:09 +0800

    refactor: schema update

--- src/weapp/generateSchema.js
+++ src/weapp/generateSchema.js
@@ -15,42 +15,42 @@ const service = new Service({
 
       const tablesEl = document.querySelectorAll('table')[tableIndex];
       Array.from(tablesEl?.querySelector('tbody').children)?.forEach((el) => {
-        const len = el.children.length;
+        if (el?.className.indexOf('children-table') > -1) {
+          return;
+        }
         const options = [];
 
-        if (el?.className.indexOf('children-table') === -1 && len > 0) {
-          const nextTrEl = el.nextElementSibling;
-          if (nextTrEl?.className.indexOf('children-table') > -1) {
-            nextTrEl
-              .querySelector('tbody')
-              .querySelectorAll('tr')
-              .forEach((el) => {
-                options.push(el.children[0].innerText);
-              });
-          }
-
-          const hasChild = tablesEl.querySelector('.children-table');
-          const defaultValue =
-            el.children[fields.defaultValue + (hasChild ? 1 : 0)]?.innerText;
-          const description =
-            el.children[fields.description + (hasChild ? 1 : 0)]?.innerText;
-          const required =
-            el.children[fields.required + (hasChild ? 1 : 0)]?.innerText;
-
-          attributes.push({
-            name: el.children[fields.name + (hasChild ? 1 : 0)]?.innerText,
-            type: handleType(
-              el.children[fields.type + (hasChild ? 1 : 0)]?.innerText,
-              '/',
-            ),
-            ...(options.length > 0 ? { options } : {}),
-            ...(defaultValue ? { defaultValue } : {}),
-            ...(description ? { description } : {}),
-            ...(required !== undefined && required !== null
-              ? { required: required === '是' }
-              : {}),
-          });
+        const nextTrEl = el.nextElementSibling;
+        if (nextTrEl?.className.indexOf('children-table') > -1) {
+          nextTrEl
+            .querySelector('tbody')
+            .querySelectorAll('tr')
+            .forEach((el) => {
+              options.push(el.children[0].innerText);
+            });
         }
+
+        const hasChild = tablesEl.querySelector('.children-table');
+        const defaultValue =
+          el.children[fields.defaultValue + (hasChild ? 1 : 0)]?.innerText;
+        const description =
+          el.children[fields.description + (hasChild ? 1 : 0)]?.innerText;
+        const required =
+          el.children[fields.required + (hasChild ? 1 : 0)]?.innerText;
+
+        attributes.push({
+          name: el.children[fields.name + (hasChild ? 1 : 0)]?.innerText,
+          type: handleType(
+            el.children[fields.type + (hasChild ? 1 : 0)]?.innerText,
+            '/',
+          ),
+          ...(options.length > 0 ? { options } : {}),
+          ...(defaultValue ? { defaultValue } : {}),
+          ...(description ? { description } : {}),
+          ...(required !== undefined && required !== null
+            ? { required: required === '是' }
+            : {}),
+        });
       });
 
       return attributes;
