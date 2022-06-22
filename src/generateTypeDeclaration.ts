188a63a91ff31719cc951f4ca7dbd8d443d39703
robin <robin@rnode.me>
2022-06-22 23:49:01 +0800

    refactor: 优化类型生成程序

--- src/generateTypeDeclaration.ts
+++ src/generateTypeDeclaration.ts
@@ -1,16 +1,29 @@
 import { compileFromFile } from "json-schema-to-typescript";
-import * as fs from "fs";
+import * as fs from "fs-extra";
 import * as path from "path";
+import * as humps from "humps";
+
 const platforms = ['alipay', 'swan', 'weapp', 'jd', 'qq', 'tt'];
 platforms.forEach((platform) => {
   const jsonPath = path.join(__dirname, `${platform}/jsonSchema`);
-  const dist = path.join(__dirname, `../dist/${platform}`);
+  const dist = path.join(__dirname, `../dist/types/${platform}`);
+  const distSchema = path.join(__dirname, `../dist/schema/${platform}`);
+
+  // copy jsonschema to dist
+  if (!fs.existsSync(distSchema)) {
+    fs.mkdirSync(distSchema, { recursive: true });
+  }
+  fs.copySync(jsonPath, distSchema);
+
+  // generate typescript
   if (!fs.existsSync(dist)) {
     fs.mkdirSync(dist, { recursive: true });
   }
   const files = fs.readdirSync(jsonPath);
+  const fileNames: string[] = []
   files.forEach((file) => {
     const filePath = path.join(jsonPath, file);
+    fileNames.push(file.replace('.json', ''));
     compileFromFile(filePath).then((ts) =>
       fs.writeFileSync(
         path.join(dist, `${file.replace('.json', '.d.ts')}`),
@@ -18,4 +31,16 @@ platforms.forEach((platform) => {
       ),
     );
   });
+  const index = path.join(dist, 'index.d.ts');
+  const code = fileNames.map(fileName => {
+    return `export { default as ${humps.pascalize(fileName)}Prop } from './${fileName}';`
+  }).join('\n');
+  fs.writeFileSync(index, code);
+
 });
+
+const code = platforms.map(platform => {
+  return `export * as ${platform} from './${platform}/index';`
+}).join('\n');
+
+fs.writeFileSync(path.join(__dirname, `../dist/types/index.d.ts`), code);
