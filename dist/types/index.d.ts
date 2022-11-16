383dc7551ed8c9be1e390aef0d14d08b4518e447
robin <robin@rnode.me>
2022-11-16 11:17:49 +0800

    release

---
+++ dist/types/index.d.ts
@@ -0,0 +1,6 @@
+export * as alipay from './alipay/index';
+export * as swan from './swan/index';
+export * as weapp from './weapp/index';
+export * as jd from './jd/index';
+export * as qq from './qq/index';
+export * as tt from './tt/index';
