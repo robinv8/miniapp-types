d4975e24e77a992ae17187199d4dedf0694a6b49
Ren Yubin <robin@sifou.com>
2023-02-06 23:40:25 +0800

    refactor: 调整同步程序

--- src/tt/generateSchema.ts
+++ src/tt/generateSchema.ts
@@ -1,79 +1,94 @@
-import Service from "../service";
-import { Attribute } from "../types";
-const typeAlias = require('../typeAlias.json')
+import Service from '../service';
+import { Attribute } from '../types';
+const typeAlias = require('../typeAlias.json');
 
 const service = new Service({
   platform: 'tt',
   waitUntil: 'networkidle0',
   evaluate: async (page, opts) => {
-    return await page.evaluate(({ tableIndex = 0, fields, typeAlias }) => {
-      function handleType(str, char) {
-        if(str.toLowerCase() === 'eventhandler') {
-          return 'eventHandle'
-        }
-        if (str.indexOf(char) === -1) {
-          return str;
-        }
-        return str.split(char).map((item) => typeAlias[item.trim()] || item.trim());
-      }
-
-      function handleDescription(str) {
-        if (!str) {
-          return ''
-        }
-        let result = str;
-        result = result.replace(/\n?默认值： .*\n?/g, '');
-        result = result.replace(/\n?版本要求： .*\n?/g, '');
-        return result;
-      }
-      const attributes: Attribute[] = [];
-
-      const tablesEl = document.querySelectorAll('table')[tableIndex];
-      tablesEl?.querySelectorAll('tbody tr')?.forEach((el) => {
-        const len = el.children.length;
-
-        if (len === 0) {
-          return
-        }
-        const options: string[] = [];
-
-        const innerText = (<HTMLElement>el.children[0])?.innerText;
-        document.querySelectorAll('table').forEach((table) => {
-          const text = table.previousElementSibling?.innerHTML;
-          if (text === `${innerText} 的合法值`) {
-            table.querySelectorAll('tbody tr').forEach((el) => {
-              const innerText = (<HTMLElement>el.children[0])?.innerText;
-              if(!innerText.match(/[\u4e00-\u9fa5]/)) {
-                options.push(innerText);
-              }
-            });
+    return await page.evaluate(
+      ({ tableIndex = 0, fields, typeAlias }) => {
+        function handleType(str, char) {
+          if (str.toLowerCase() === 'eventhandler') {
+            return 'eventHandle';
           }
-        });
-        const name = (<HTMLElement>el.children[fields.name])?.innerText;
-
-        if (name.match(/[\u4e00-\u9fa5]/)) {
-          return
+          if (str.indexOf(char) === -1) {
+            return str;
+          }
+          return str
+            .split(char)
+            .map((item) => typeAlias[item.trim()] || item.trim());
         }
-        const type = handleType((<HTMLElement>el.children[fields.type])?.innerText, '/');
-        const defaultValue = (<HTMLElement>el.children[fields.defaultValue])?.innerText;
-        const description = (<HTMLElement>el.children[fields.description])?.innerText;
-        const required = (<HTMLElement>el.children[fields.required])?.innerText;
-        const filteredDescription = handleDescription(description);
 
-        attributes.push({
-          name,
-          type,
-          ...(options.length > 0 ? { options } : {}),
-          ...(defaultValue && defaultValue !== '\n' ? { defaultValue } : {}),
-          ...(filteredDescription ? { description: filteredDescription } : {}),
-          ...(required !== undefined && required !== null
-            ? { required: required === '是' }
-            : {}),
+        function handleDescription(str) {
+          if (!str) {
+            return '';
+          }
+          let result = str;
+          result = result.replace(/\n?默认值： .*\n?/g, '');
+          result = result.replace(/\n?版本要求： .*\n?/g, '');
+          return result;
+        }
+        const attributes: Attribute[] = [];
+
+        const tablesEl = document.querySelectorAll('table')[tableIndex];
+        tablesEl?.querySelectorAll('tbody tr')?.forEach((el) => {
+          const len = el.children.length;
+
+          if (len === 0) {
+            return;
+          }
+          const options: string[] = [];
+
+          const innerText = (<HTMLElement>el.children[0])?.innerText;
+          document.querySelectorAll('table').forEach((table) => {
+            const text = table.previousElementSibling?.innerHTML;
+            if (text === `${innerText} 的合法值`) {
+              table.querySelectorAll('tbody tr').forEach((el) => {
+                const innerText = (<HTMLElement>el.children[0])?.innerText;
+                if (!innerText.match(/[\u4e00-\u9fa5]/)) {
+                  options.push(innerText);
+                }
+              });
+            }
+          });
+          const name = (<HTMLElement>el.children[fields.name])?.innerText;
+
+          if (name.match(/[\u4e00-\u9fa5]/)) {
+            return;
+          }
+          const type = handleType(
+            (<HTMLElement>el.children[fields.type])?.innerText,
+            '/',
+          );
+          const defaultValue = (<HTMLElement>el.children[fields.defaultValue])
+            ?.innerText;
+          const description = (<HTMLElement>el.children[fields.description])
+            ?.innerText;
+          const required = (<HTMLElement>el.children[fields.required])
+            ?.innerText;
+          const filteredDescription = handleDescription(description);
+
+          attributes.push({
+            name,
+            type,
+            ...(options.length > 0 ? { options } : {}),
+            ...(defaultValue && defaultValue !== '\n' && defaultValue !== '无'
+              ? { defaultValue }
+              : {}),
+            ...(filteredDescription
+              ? { description: filteredDescription }
+              : {}),
+            ...(required !== undefined && required !== null
+              ? { required: required === '是' }
+              : {}),
+          });
         });
-      });
 
-      return attributes;
-    }, { ...opts, typeAlias });
+        return attributes;
+      },
+      { ...opts, typeAlias },
+    );
   },
 });
 
